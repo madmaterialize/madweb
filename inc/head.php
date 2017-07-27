6a9073f26d61c13ef4bb3bf995d5efe457ca0d39
madmaterialize <madmaterialize@gmail.com>
2017-07-27 09:50:48 +0600

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -1,4 +1,11 @@
 <head>
-
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- google material design cdn -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <!-- custom css -->
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
